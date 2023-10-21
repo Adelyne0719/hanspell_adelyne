2d46452583a0d06a9f5c18d40f8f5987fbe77db7
Adelyne0719 <125781188+Adelyne0719@users.noreply.github.com>
2023-10-22 01:54:12 +0900

    Add files via upload

---
+++ hanspell_adelyne/spell_checker.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+"""
+Python용 한글 맞춤법 검사 모듈
+"""
+
+import requests
+import json
+import time
+import sys
+import re
+import os
+from collections import OrderedDict
+import xml.etree.ElementTree as ET
+
+from . import __version__
+from .response import Checked
+from .constants import base_url
+from .constants import CheckResult
+from urllib import parse
+
+_agent = requests.Session()
+PY3 = sys.version_info[0] == 3
+
+def read_token():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    token_file_path = os.path.join(current_directory, "token.txt")
+    
+    with open(token_file_path, "r") as f:
+        TOKEN = f.read()
+    
+    return TOKEN
+
+def update_token(agent):
+    """update passportkey
+    from https://gist.github.com/AcrylicShrimp/4c94db38b7d2c4dd2e832a7d53654e42
+    """
+
+    html = agent.get(url='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=맞춤법검사기') 
+
+    match = re.search('passportKey=([a-zA-Z0-9]+)', html.text)
+    if match is not None:
+        TOKEN = parse.unquote(match.group(1))
+        with open("token.txt", "w") as f:
+            f.write(TOKEN)
+    return TOKEN
+
+def _remove_tags(text):
+    text = u'<content>{}</content>'.format(text).replace('<br>','')
+    if not PY3:
+        text = text.encode('utf-8')
+
+    result = ''.join(ET.fromstring(text).itertext())
+
+    return result
+
+def _get_data(text, token):
+    payload = {
+        "_callback": "window.__jindo2_callback._spellingCheck_0",
+        "q": text,
+        "color_blindness": 0,
+        "color_blindness": 0,
+        "passportKey": token
+    }
+
+    headers = {
+        "Host": "m.search.naver.com",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.9 Safari/537.36",
+        "referer": "https://search.naver.com/",
+        "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Accept": "*/*"
+    }
+
+    start_time = time.time()
+    r = _agent.get(base_url, params=payload, headers=headers)
+
+    passed_time = time.time() - start_time
+
+    r = r.text[42:-2]
+
+    data = json.loads(r)
+    return passed_time, data
+
+def check(text):
+    """
+    매개변수로 입력받은 한글 문장의 맞춤법을 체크합니다.
+    """
+    if isinstance(text, list):
+        result = []
+        for item in text:
+            checked = check(item)
+            result.append(checked)
+        return result
+
+    # 최대 500자까지 가능.
+    if len(text) > 500:
+        return Checked(result=False)
+
+    TOKEN = read_token()
+    passed_time, data = _get_data(text, TOKEN)
+    if "error" in data["message"].keys():
+        TOKEN = update_token(_agent)
+        passed_time, data = _get_data(text, TOKEN)
+        if "error" in data["message"].keys():
+            return Checked(result=False)
+    
+    html = data['message']['result']['html']
+    result = {
+        'result': True,
+        'original': text,
+        'checked': _remove_tags(html),
+        'errors': data['message']['result']['errata_count'],
+        'time': passed_time,
+        'words': OrderedDict(),
+    }
+
+    # 띄어쓰기로 구분하기 위해 태그는 일단 보기 쉽게 바꿔둠.
+    # ElementTree의 iter()를 써서 더 좋게 할 수 있는 방법이 있지만
+    # 이 짧은 코드에 굳이 그렇게 할 필요성이 없으므로 일단 문자열을 치환하는 방법으로 작성.
+    html = html.replace('<em class=\'green_text\'>', '<green>') \
+               .replace('<em class=\'red_text\'>', '<red>') \
+               .replace('<em class=\'violet_text\'>', '<violet>') \
+               .replace('<em class=\'blue_text\'>', '<blue>') \
+               .replace('</em>', '<end>')
+    items = html.split(' ')
+    words = []
+    tmp = ''
+    for word in items:
+        if tmp == '' and word[:1] == '<':
+            pos = word.find('>') + 1
+            tmp = word[:pos]
+        elif tmp != '':
+            word = u'{}{}'.format(tmp, word)
+        
+        if word[-5:] == '<end>':
+            word = word.replace('<end>', '')
+            tmp = ''
+
+        words.append(word)
+
+    for word in words:
+        check_result = CheckResult.PASSED
+        if word[:5] == '<red>':
+            check_result = CheckResult.WRONG_SPELLING
+            word = word.replace('<red>', '')
+        elif word[:7] == '<green>':
+            check_result = CheckResult.WRONG_SPACING
+            word = word.replace('<green>', '')
+        elif word[:8] == '<violet>':
+            check_result = CheckResult.AMBIGUOUS
+            word = word.replace('<violet>', '')
+        elif word[:6] == '<blue>':
+            check_result = CheckResult.STATISTICAL_CORRECTION
+            word = word.replace('<blue>', '')
+        result['words'][word] = check_result
+
+    result = Checked(**result)
+
+    return result
